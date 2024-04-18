081985da0e5546d5d7ea022b23ecb39ba2f7c540
Vanessa Cavaco <90183816+VanessaCavaco@users.noreply.github.com>
2024-04-18 09:14:13 +0200

    Getting Parameter data from the txt using Revit API

--- RevitAddins/ReadTxt.cs
+++ RevitAddins/ReadTxt.cs
@@ -34,8 +34,7 @@ namespace RevitAddins
                 // Call method to set and open the external shared parameter file
                 // set the path of shared parameter file to current Revit
                 app.SharedParametersFilename = sharedParameterFileName;
-                // open the file
-               DefinitionFile file=  app.OpenSharedParameterFile();
+                DefinitionFile file =  app.OpenSharedParameterFile();
 
                 StringBuilder fileInformation = new StringBuilder(500);
 
@@ -54,17 +53,43 @@ namespace RevitAddins
                         // Parameter as a definition properties
                         fileInformation.AppendLine("Definition Name: " + definition.Name 
                             + "\n" + "Definition Parameter Group id " + definition.GetGroupTypeId()
-                            + "\n" + "Definition Parameter DataType " + definition.GetDataType()
-                            + "\n" + "Definition Parameter Type " + definition.GetType().Name
+                            + "\n" + "Definition Parameter Get DataType " + definition.GetDataType().TypeId.Length
+                            + "\n" + "Definition Parameter Type " + definition.GetType()
                             + "\n" + "Definition Parameter depricated Prameter Group " + definition
                             + "\n" + "Definition Parameter Hash Code " + definition.GetHashCode()
                             );
                         // Parameter as a definition as External Definition.
                         var param = definition as ExternalDefinition;
+                        var paramForgeType = (ForgeTypeId)param.GetDataType();
+                        //check if the parameter type is family type
+                        var isCat = Category.IsBuiltInCategory(paramForgeType);
+                        try
+                        {
+                            var Cat = Category.GetBuiltInCategory(paramForgeType);
+                            TaskDialog.Show("cat", Cat.ToString());
+                        }
+                        catch (Exception e)
+                        {
+                            TaskDialog.Show("Error", e.ToString());
+                        }
+                        
+                        //check if the parameter type is mesurable
+                        var isMes = UnitUtils.IsMeasurableSpec(paramForgeType);
+                        IList<ForgeTypeId> fType = UnitUtils.GetAllMeasurableSpecs();
+                        for (int i = 0; i < fType.Count; i++)
+                        {
+                            var f = fType[i];
+                            if (f == paramForgeType) {
+                                TaskDialog.Show("ftype", f.TypeId.ToString());
+                            }
+                        }
+
                         fileInformation.AppendLine("\n"+"ExtDefinition Name: " + param.Name
                            + "\n" + "ExtDefinition Parameter  GUID " + param.GUID
-                           + "\n" + "ExtDefinition Parameter DataType " + param.GetDataType()
-                           + "\n" + "ExtDefinition Parameter Type " + param.GetType().Name
+                           + "\n" + "ExtDefinition Parameter  data type is category? " + isCat
+                           + "\n" + "ExtDefinition Parameter  data type is mesurable? " + isMes
+                           + "\n" + "ExtDefinition Parameter DataType Length" + paramForgeType.TypeId.Length
+                           + "\n" + "ExtDefinition Parameter Type/Parameter group " + param.ParameterGroup
                            + "\n" + "ExtDefinition Parameter Group id " + param.GetGroupTypeId()
                            + "\n" + "ExtDefinition Parameter Owner Group " + param.OwnerGroup
                            + "\n" + "ExtDefinition Parameter Hide when no value " + param.HideWhenNoValue
@@ -87,37 +112,6 @@ namespace RevitAddins
                 TaskDialog.Show("Error", e.ToString());
                 return Result.Failed;
             }
-            //try
-            //{
-            //    //creates a sample of StreamReader with using:
-            //    string txtPath = @"D:\GitHubRepos\RevitAddins\RevitAddins\Assets\mvp-fullstack-sample.txt";
-            //    using (StreamReader reader = new StreamReader(txtPath))
-            //    {
-            //        //txt to string
-            //        string txtFileText = reader.ReadToEnd();
-
-            //        var sharedParameters = new SharedParametersFile.SharedParameterFile(txtFileText);
-            //        //string parameters = "";
-            //        //for (int i = 0; i> sharedParameters.Parameters.Count; i++) { 
-            //        //    parameters += sharedParameters.Parameters[i].Name;
-            //        //    parameters += "\n";
-            //        //}
-            //        //TaskDialog.Show("Only Parameters Parameters", parameters);
-
-            //        TaskDialog.Show("Shared Parameters",sharedParameters.ToString());
-
-            //        TaskDialog.Show("Txt file", txtFileText);
-            //    }
-            //    return Result.Succeeded;
-            //}
-            //catch (Exception e)
-            //{
-            //    TaskDialog.Show("Error", e.ToString());
-            //    return Result.Failed;
-            //}
-
-
-
         }
 
     }
